5e5e68e1bd53eaf4394ac3f77cf0de4468fda4ce
Smith <smithoo@nhnnext.org>
2015-06-19 03:24:06 +0900

    web_animation

---
+++ [Week8] WEB_Animation/WEB_Animation.js
@@ -0,0 +1,37 @@
+var box = document.getElementById('box');
+box.style.left = '10px';
+
+function goRightWithTimeout() {
+	box.style.left = parseInt(box.style.left) + 5 + 'px';
+	setTimeout(goRightWithTimeout, 20);
+}
+//setTimeout(goRightWithTimeout, 20);
+
+function goRightWithInterval() {
+	box.style.left = parseInt(box.style.left) + 5 + 'px';
+}
+//setInterval(goRightWithInterval, 20);
+
+function goRightWithRAF (timestamp) {
+	console.log(timestamp);
+	box.style.left = parseInt(box.style.left) + 5 + 'px';
+	requestAnimationFrame(goRightWithRAF);
+}
+//requestAnimationFrame(goRightWithRAF);
+
+
+var startTime;
+var startLeft = parseInt(box.style.left);
+var unitDist = (800 - parseInt(box.style.left)) / 5000;
+
+function tick(timestamp) {
+	if (!startTime) startTime = timestamp;
+	var deltaTime = timestamp - startTime;
+
+	box.style.left = Math.floor(Math.min(startLeft + unitDist * deltaTime, 800)) + 'px';
+
+	if (parseInt(box.style.left) < 800) {
+		requestAnimationFrame(tick);
+	}
+}
+//requestAnimationFrame(tick);
